ced6d4d10f2e7511d0a667f98887cc4af0c2be00
Christoph Honal <christoph.honal@web.de>
2023-08-25 01:50:10 +0200

    Migrate scripts

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM mathworks/matlab:r2023a
+
+USER root
+
+RUN apt-get update && \
+    apt-get upgrade -y && \
+    apt-get install -y mesa-utils software-properties-common && \
+    add-apt-repository -y ppa:kisak/kisak-mesa && \
+    apt-get update && \
+    apt-get dist-upgrade -y && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN mkdir -p /opt/matlab && \
+    chown -R matlab:matlab /opt/matlab
+
+COPY ./drirc /home/matlab/.drirc
+COPY ./drirc /etc/drirc
+COPY ./java.opts /opt/matlab/R2023a/bin/glnxa64/java.opts
+COPY ./entrypoint.sh /home/matlab/entrypoint.sh
+
+USER matlab
---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd "${0%/*}"
+
+docker build -t stargate01/matlab:r2023a-iris .
+
+GID_VIDEO=$(getent group video | awk -F: '{printf "%d\n", $3}')
+GID_RENDER=$(getent group render | awk -F: '{printf "%d\n", $3}')
+
+SOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+docker run -it --net=host --ipc=host --privileged -v $HOME:$HOME -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -e DISPLAY=$DISPLAY --user=$(id -u):$(id -g) --group-add $GID_VIDEO --group-add $GID_RENDER --shm-size=4G stargate01/matlab:r2023a-iris /home/matlab/entrypoint.sh "$@"
